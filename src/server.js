183dde95ae6faa136c0019765e6c1b5ca521cb7f
NguyenVinh2504 <vinhhoang010190@gmail.com>
2023-10-26 00:53:35 +0700

    oki

--- src/server.js
+++ src/server.js
@@ -1,11 +1,32 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
 import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
 const app = express()
+
+const hostname = 'localhost'
 const port = 2504
 
-app.get('/', function (req, res) {
-    res.send('<h1>Xin chao</h1>')
-  })
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  console.log(mapOrder(
+    [ { id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' } ],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
+})
+
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
+})
