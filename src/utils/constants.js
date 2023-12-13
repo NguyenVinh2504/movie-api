594b7b1e6bacbf2cd93cefc14f01c356aaf6c5a8
NguyenVinh2504 <vinhhoang010190@gmail.com>
2023-12-13 21:33:58 +0700

    update WHITELIST_DOMAINS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@ export const WHITELIST_DOMAINS = [
   'https://viejoy.vercel.app',
   'http://localhost:3000',
   'https://viejoy.site',
-  'https://www.viejoy.site/'
+  'https://www.viejoy.site'
   // 'https://nguyenvinh2504.github.io'
 ]
